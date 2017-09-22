fd414961c4bf7209bebd43d8ffb8c9670498335e
Farki93 <cph-if36@cphbusiness.com>
2017-09-22 10:41:27 +0200

    Exercise 1

--- Thread1/src/main/java/Thread/Counter.java
+++ Thread1/src/main/java/Thread/Counter.java
@@ -12,26 +12,48 @@ package Thread;
  * @author Farkas
  */
 public class Counter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         
     
     Thread t1 = new Thread(()->{
         long sum = 0;
-        for (int i = 1; i <= 10; i++ ){
+        for (int i = 1; i <= 1e6; i++ ){
             sum += i;
-            System.out.println(sum);
+          
         }
+        System.out.println("Thread1 sum: "+sum);
     });
     
     
     Thread t2 = new Thread(()->{
-        
+       
+        int num = 1;
+        while(num <= 5){
+        System.out.println("Thread2: "+num);
+      num++;
+      try {
+        Thread.sleep(2000);        
+      } catch (InterruptedException ex) {}
+    }
     });
     
     Thread t3 = new Thread(()->{
-        
+        int numb = 10;
+        while(numb > 9){
+        System.out.println("Thread 3: "+numb);
+      numb++;
+      try {
+        Thread.sleep(3000);        
+      } catch (InterruptedException ex) {}
+    }
     });
     
     t1.start();
+    t2.start();
+    t3.start();
+    
+    t1.join();
+    t2.join(1000);
+    t3.join(10000);
     }
 }
